0ec51f379c1c01c191f2db413fd834e1aafc4dd1
Emil Nikov <contacts@enstyled.com>
2021-02-16 21:56:38 +0100

    POC with browserless

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
-module.exports = (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-    })
+const browserless = require('browserless')()
+
+module.exports = async (req, res) => {
+    // Default settings
+    const defaults = {
+        ignoreHTTPSErrors: true,
+        args: ['--disable-gpu', '--single-process', '--no-zygote', '--no-sandbox', '--hide-scrollbars']
+    }
+
+    // Request settings
+    let settings = {
+        device: 'iPhone 12'
+    }
+
+    // The requested URL
+    const { url = 'http://google.com' } = req.query
+
+    // Take the screenshot
+    const buffer = await browserless.screenshot(url, {...defaults, ...settings})
+
+    res.setHeader('content-type', 'image/png');
+    res.send(buffer)
 }
