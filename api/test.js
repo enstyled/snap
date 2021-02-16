2def1883b2cf03276b8886c22d1d9d35a4a5eea7
Emil Nikov <contacts@enstyled.com>
2021-02-16 23:13:27 +0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,3 +1,8 @@
+const browserless = require('browserless')({
+    ignoreHTTPSErrors: true,
+    args: ['--disable-gpu', '--single-process', '--no-zygote', '--no-sandbox', '--hide-scrollbars']
+})
+
 module.exports = (req, res) => {
   const { name = 'World' } = req.query
   res.send(`Hello ${name}!`)
